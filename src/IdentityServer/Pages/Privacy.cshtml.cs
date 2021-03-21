3be454f2abd18e0e90e3e716d635ebb5de537970
PhantomCloak <unalozyurtrooter@hotmail.com>
2021-03-21 22:50:40 +0300

    Add UI pages

---
+++ src/IdentityServer/Pages/Privacy.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IdentityServer.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
